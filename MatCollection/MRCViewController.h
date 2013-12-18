efa0746be4d87749d66e64adeb86cf43ccf292b6
Mathew Cruz <jonusx@gmail.com>
2013-12-18 16:35:03 -0500

    Initial Commit

---
+++ MatCollection/MRCViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRCViewController.h
+//  MatCollection
+//
+//  Created by mathew cruz on 12/18/13.
+//  Copyright (c) 2013 mathew cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRCViewController : UIViewController
+
+@end
---
+++ MatCollectionTests/MatCollectionTests.m
@@ -0,0 +1,34 @@
+//
+//  MatCollectionTests.m
+//  MatCollectionTests
+//
+//  Created by mathew cruz on 12/18/13.
+//  Copyright (c) 2013 mathew cruz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MatCollectionTests : XCTestCase
+
+@end
+
+@implementation MatCollectionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
